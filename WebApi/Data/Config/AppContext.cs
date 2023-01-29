9ae9b27a63457eb8cf6651489cf850eb6c0427b0
Lucas Alves de Abreu <52301776+lucas-Alves26@users.noreply.github.com>
2023-01-29 15:42:11 -0300

    update created context DB

---
+++ WebApi/Data/Config/AppContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model.Product;
+
+namespace Data.Config
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConection());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        private string GetStringConection()
+        {
+            return "";
+        }
+    }
+}
---
+++ WebApi/Model/Product/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model.Product
+{
+    [Table("produto")]
+    public class Product
+    {
+        [Key]
+        [Column("id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("nome")]
+        [Display(Name = "Nome")]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [Column("quantity")]
+        [Display(Name = "Quantidade")]
+        [MaxLength(255)]
+        public string Quantity { get; set; }
+    }
+}
